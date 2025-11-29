6cb2945cf8b4698ddd290cd20fc2664f38c09f03
HungTran170904 <tienhung17092004@gmail.com>
2025-11-29 09:29:38 +0700

    fix: fix security vulnerabilities in backend

--- dkhp_backend/src/main/java/university/DemoApplication.java
+++ dkhp_backend/src/main/java/university/DemoApplication.java
@@ -1,40 +1,10 @@
 package university;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Optional;
-
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.Environment;
-
-import com.google.gson.Gson;
-
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
-import university.DTO.CourseDTO;
-import university.DTO.SubjectDTO;
-import university.DTO.UserDTO;
-import university.DTO.Converter.CourseConverter;
-import university.Model.Course;
-import university.Model.Semester;
-import university.Model.Student;
-import university.Model.Subject;
-import university.Model.User;
-import university.Repository.CourseRepo;
-import university.Repository.SemesterRepo;
-import university.Repository.StudentRepo;
-import university.Repository.SubjectRepo;
-import university.Repository.UserRepo;
-import university.Service.AdminService;
 import university.Service.SseService;
-import university.Service.StudentService;
-import university.Util.OpeningRegPeriods;
 
 
 @SpringBootApplication
